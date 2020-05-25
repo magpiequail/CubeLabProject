9851dec92edbf2dd24e9e0542974554c55e81acb
magpiequail <48472215+magpiequail@users.noreply.github.com>
2020-05-25 18:17:30 +0900

    stageselect

--- CubeLabProject/Assets/Scripts/LevelSelectButton.cs
+++ CubeLabProject/Assets/Scripts/LevelSelectButton.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelSelectButton : MonoBehaviour
 {
     public int buildIndex;
+    Image image;
     public Sprite oneStar;
     public Sprite twoStars;
     public Sprite threeStars;
 
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerPrefs.GetInt("" + buildIndex + "stars");
+        if (PlayerPrefs.GetInt("" + buildIndex + "stars" )== 1)
+        {
+            image.sprite = oneStar;
+        }
+        else if (PlayerPrefs.GetInt("" + buildIndex + "stars") == 2)
+        {
+            image.sprite = twoStars;
+        }
+        else if (PlayerPrefs.GetInt("" + buildIndex + "stars") == 3)
+        {
+            image.sprite = threeStars;
+        }
     }
 
     // Update is called once per frame
@@ -20,5 +40,10 @@ public class LevelSelectButton : MonoBehaviour
     {
         PlayerPrefs.GetInt("" + buildIndex + "stars");
         
+    }
+    
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene(buildIndex);
     }
 }
